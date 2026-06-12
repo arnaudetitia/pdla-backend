26fc723ce07413084a61e74fb349fdfa3b9b13fe
arnaudetitia <arnaud.etitia@gmail.com>
2026-06-12 21:01:41 +0200

    route recuperation des questions

--- app.ts
+++ app.ts
@@ -1,16 +1,37 @@
 import express, { Application } from "express";
 import { createServer } from "http";
+import { QuestionController } from "./controllers/question.controller";
 
 export class App {
   app: Application;
+  questionController: QuestionController;
   httpServer: any;
   constructor() {
     this.app = express();
     this.httpServer = createServer(this.app);
+    this.questionController = new QuestionController();
     this.routes();
   }
 
-  private routes(): void {}
+  private routes(): void {
+    this.app.get("/questions", async (req, res) => {
+      try {
+        const question = await this.questionController.getAllQuestions();
+        res.json(
+          question.map((q) => {
+            return {
+              ...q,
+              id: Number.parseInt(q.id),
+              annee: Number.parseInt(q.annee),
+            };
+          }),
+        );
+      } catch (error) {
+        console.error("Erreur lors de la récupération des questions:", error);
+        res.status(500).json({ error: "Erreur serveur" });
+      }
+    });
+  }
 
   public listen(port: number): void {
     this.httpServer.listen(port, () => {
