caaf515f145eb20261b5dec854ada8658814fa7f
arnaudetitia <arnaud.etitia@gmail.com>
2026-07-02 09:21:08 +0200

    enregistrement des equipes

---
+++ controllers/equipe.controller.ts
@@ -0,0 +1,25 @@
+import database = require("../database");
+
+export class EquipeController {
+  public async enregistrerEquipes(equipes: string[]) {
+    const pool = database.Database.getPool();
+    try {
+      await pool.query("BEGIN");
+      await pool.query("TRUNCATE TABLE pdla.equipes");
+
+      for (const [index, equipe] of equipes.entries()) {
+        await pool.query({
+          text: `
+                INSERT INTO pdla.equipes(nom_equipe, ordre)
+                VALUES ($1, $2)
+                `,
+          values: [equipe, index + 1],
+        });
+      }
+
+      await pool.query("COMMIT");
+    } catch (error) {
+      await pool.query("ROLLBACK");
+    }
+  }
+}
