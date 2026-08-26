7add4450aee677efb5982a86d198a0087834bac7
arnaudetitia <arnaud.etitia@gmail.com>
2026-08-26 18:23:35 +0200

    config pour render

--- database.ts
+++ database.ts
@@ -1,19 +1,20 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
 export class Database {
   private static pool: Pool;
 
   public static getPool(): Pool {
+    dotenv.config({ path: `environments/environment.${process.env.NODE_ENV}` });
     if (!this.pool) {
       this.pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'pile_dans_lannee_db',
-        password: 'admin',
-        max: 20,
-        port: 5432,
+        connectionString: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "prod"
+            ? { rejectUnauthorized: false }
+            : false,
       });
-      console.log('🐘 Connexion à PostgreSQL établie');
+      console.log("🐘 Connexion à PostgreSQL établie");
     }
     return this.pool;
   }
